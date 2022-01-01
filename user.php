8844089115bab607575d6a82cab65395e174c3f8
BAIQEVIPRATIWI <baiqevy25@gmail.com>
2022-01-01 16:14:09 +0800

    edit

--- user.php
+++ user.php
@@ -1,10 +1,5 @@
 <?php
 require_once('header_template.php');
-<<<<<<< HEAD
-?>
-
-<h3>Halaman User</h3>
-=======
 $data = mysqli_query($host, "SELECT * FROM user");
 ?>
 
@@ -16,21 +11,26 @@ $data = mysqli_query($host, "SELECT * FROM user");
             <div class="table-responsive">
                 <table class="table w-100">
                     <thead>
-                        <th>No</th>
-                        <th>Nama User</th>
-                        <th>Level</th>
-                        <th>Aksi</th>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama User</th>
+                            <th>Level</th>
+                            <th>Aksi</th>
+                        </tr>
                     </thead>
                     <tbody>
                         <?php $i = 1;
                         foreach ($data as $row) : ?>
-                            <td><?= $i++ ?></td>
-                            <td><?= $row['username'] ?></td>
-                            <td><?= $row['level'] ?></td>
-                            <td>
-                                <a href="userEdit.php?id=<?= $row['id'] ?>" class="btn btn-warning">Edit</a>
-                                <a href="userHapus.php?id=<?= $row['id'] ?>" class="btn btn-danger">Hapus</a>
-                            </td>
+                            <tr>
+
+                                <td><?= $i++ ?></td>
+                                <td><?= $row['username'] ?></td>
+                                <td><?= $row['level'] ?></td>
+                                <td>
+                                    <a href="userEdit.php?id=<?= $row['id'] ?>" class="btn btn-warning">Edit</a>
+                                    <a href="userHapus.php?id=<?= $row['id'] ?>" class="btn btn-danger">Hapus</a>
+                                </td>
+                            </tr>
                         <?php endforeach; ?>
                     </tbody>
                 </table>
@@ -38,7 +38,6 @@ $data = mysqli_query($host, "SELECT * FROM user");
         </div>
     </div>
 </div>
->>>>>>> bahry
 
 <?php
 require_once('footer_template.php');
