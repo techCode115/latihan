0aab49a68773a72800af0f3f16ae257a8b4ec2f9
BAIQEVIPRATIWI <baiqevy25@gmail.com>
2022-01-01 15:59:58 +0800

    userEdit.php

---
+++ evy.php
@@ -0,0 +1,9 @@
+<?php
+require_once('header_template.php');
+?>
+
+
+
+<?php
+require_once('footer_template.php');
+?>
---
+++ userEdit.php
@@ -0,0 +1,18 @@
+<?php
+require_once('header_template.php');
+?>
+
+
+<form>
+    <label for="username">username:</label>
+    <input type="username" id="username">
+    <br>
+    <label for="password">password :</label>
+    <input type="password" id="password ">
+    <br>
+    <button type="submit">edit</button>
+</form>
+
+<?php
+require_once('footer_template.php');
+?>
